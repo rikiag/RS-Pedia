4eec93ba622ba9f63d364a18d104fe59d87772b5
Riki Agusnaidi <r.agusnaidi@gmail.com>
2018-05-02 15:25:36 +0700

    POST: add migration

---
+++ database/migrations/1525266865424_posts_schema.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class PostsSchema extends Schema {
+  up () {
+    this.create('posts', (table) => {
+      table.increments()
+      table.string('title')
+      table.string('slug')
+      table.text('body')
+      table.string('featured_img')
+      table.integer('user_id').unsigned().references('id').inTable('users')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('posts')
+  }
+}
+
+module.exports = PostsSchema
