c8414da13934c5ce16902e6c295332afc2c03c57
Riki Agusnaidi <r.agusnaidi@gmail.com>
2018-06-03 00:04:04 +0700

    CRUD Post - Memasukan Data

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const { validate } = use('Validator')
 const Post = use('App/Models/Post')
 
 class PostController {
@@ -8,8 +9,26 @@ class PostController {
 		return view.render('posts.add')
 	}
 
-	async store({ view, params }){
-		return view.render('dashboard.index')
+	async store({ request, response, session }){
+		const post = new Post()
+
+		post.title        = request.input('title')
+		post.place_id     = request.input('place_id')
+		post.slug         = request.input('title')
+		post.body         = request.input('body')
+		post.featured_img = request.input('featured_img')
+		post.user_id      = request.input('id')
+
+		await post.save()
+
+		session.flash({
+			notification: {
+				type: 'success',
+				message: 'Post Added!'
+			}
+		})
+
+		return response.redirect('/dashboard')
 	}
 }
 
