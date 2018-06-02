8ee353b27f4595687ba7971c38bc1116caaa16af
Riki Agusnaidi <r.agusnaidi@gmail.com>
2018-06-03 01:26:52 +0700

    CRUD Post - Upload gambar

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,7 +1,8 @@
 'use strict'
 
-const { validate } = use('Validator')
 const Post = use('App/Models/Post')
+const Helpers = use('Helpers')
+
 
 class PostController {
 
@@ -10,13 +11,26 @@ class PostController {
 	}
 
 	async store({ request, response, session }){
+
+		const featuredImg = request.file('featured_img', {
+			types: ['image'],
+			size: '2mb'
+		})
+
+		await featuredImg.move(Helpers.publicPath('img/posts'))
+
+		if (!featuredImg.moved()) {
+			return featuredImg.error()
+		}
+		return 'File moved'
+
 		const post = new Post()
 
 		post.title        = request.input('title')
 		post.place_id     = request.input('place_id')
 		post.slug         = request.input('title')
 		post.body         = request.input('body')
-		post.featured_img = request.input('featured_img')
+		post.featured_img = featuredImg.clientName
 		post.user_id      = request.input('id')
 
 		await post.save()
