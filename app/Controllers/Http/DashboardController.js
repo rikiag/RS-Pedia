5ed357fbce8c2089fdd87050b56f44fed5ba21d5
Riki Agusnaidi <r.agusnaidi@gmail.com>
2018-06-03 02:42:14 +0700

    CRUD Post - View pada dashboard

--- app/Controllers/Http/DashboardController.js
+++ app/Controllers/Http/DashboardController.js
@@ -1,8 +1,15 @@
 'use strict'
 
+const Post = use('App/Models/Post')
+const Database = use('Database')
+
 class DashboardController {
-	index({view}){
-		return view.render('dashboard.index')
+	async index({view, auth}){
+		const posts = await Database.from('posts').where('user_id', auth.user.id)
+
+		return view.render('dashboard.index', {
+			posts: posts
+		})
 	}
 }
 
